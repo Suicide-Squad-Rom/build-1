d28ba66de9a9520d1aa1195e455e3468e734a931
martinusbe <martinusbe@gmail.com>
2015-11-05 16:36:54 +0100

    add a lill flair
    Signed-off-by: martinusbe <martinusbe@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -620,6 +620,15 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   system_progress = 0.75
 
+  script.Print("#           ( (                   #");
+  script.Print("#      ( ( ) )\( )\ ) (           #");
+  script.Print("#   )\ )\( /(((_)\ (()/( ))\ (    #");
+  script.Print("# ((_)((_)(_))_((_) ((_))((_))\   #");
+  script.Print("#  \ \ / ((_)_| |(_) _| (_))(((_) #");
+  script.Print("#   \ V // _` | || / _` | || (_-< #");
+  script.Print("#    \_/ \__,_|_||_\__,_|\_,_/__/ #");
+  script.Print("#        Validus-MarshMallow      #");
+
   if OPTIONS.wipe_user_data:
     system_progress -= 0.1
   if HasVendorPartition(input_zip):
