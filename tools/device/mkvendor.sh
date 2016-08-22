51b5e6e3749ebca8ff9938ea05a742b4540fae19
Blackjubel <blackjubels@gmail.com>
2016-08-22 22:42:04 +0200

    Update mkvendor.sh

--- tools/device/mkvendor.sh
+++ tools/device/mkvendor.sh
@@ -110,4 +110,6 @@ popd
 
 echo Done!
 echo Use the following command to set up your build environment:
-echo '  'lunch cm_$DEVICE-eng
+echo '  'lunch candy_$DEVICE-eng
+echo And use the follwowing command to build a recovery:
+echo '  '. build/tools/device/makerecoveries.sh candy_$DEVICE-eng
